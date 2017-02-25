c4fc5f25e50c4184910b0a67042e470c1290b455
Anthony Huang <anthuang@umich.edu>
2017-02-24 23:00:31 -0600

    added some base files

---
+++ js/index.js
@@ -0,0 +1,3 @@
+window.onload = function(){
+	document.getElementsByTagName("h1")[0].innerHTML = 'Resize the browser window. When the width of this document is less than 500 pixels, the background-color is "lightblue", otherwise it is "lightgreen".';
+}
