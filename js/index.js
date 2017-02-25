c3d850210e89f34f36cbe3075e6bb2a05cc3d990
Anthony Huang <anthuang@umich.edu>
2017-02-25 13:55:33 -0600

    Removing works kinda.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
-window.onload = function() {
-	function clearField(input) {
-    input.value = "";
-	};
+function clearField(input) {
+  input.value = "";
+};
 
+function remove_post(key) {
+	var firebase_ref = firebase.database().ref().child("Posts").child(key);
+	firebase_ref.remove();
+	// how to remove the html depends on whether we want it to refresh or not
+	window.location.replace("index.html");
+};
+
+window.onload = function() {
 	/*
 	 * Switching tabs
 	 */
@@ -13,13 +20,13 @@ window.onload = function() {
 
 	var curr_user;
 	firebase.auth().onAuthStateChanged(user => {
-		if(user){
+		if (user) {
 			curr_user = firebase.auth().currentUser;
 		} else {
-			window.location.replace("not_logged_in.html")
+			window.location.replace("not_logged_in.html");
 		}
 	});
-	console.log(curr_user);
+	// console.log(curr_user);
 	var arr = document.getElementsByClassName("nav_el");
 	var names = ["post", "announcement", "meeting", "user"];
 	var last_active = 0;
@@ -103,44 +110,33 @@ window.onload = function() {
 	  // console.log(snapshot.val());
 	});
 
-	function remove_post(key) {
-		var firebase_ref = firebase.database().ref().child("Posts").child(key);
-		firebase_ref.remove();
-		// how to remove the html depends on whether we want it to refresh or not
-	}
-
 	// Triggers when a post is added
 	// Receive a snap which is one post
 	firebase_ref.child("Posts").on('child_added', snap => {
 		var title = snap.child("title").val();
 		var text = snap.child("text").val();
 		var select = snap.child("select").val();
+		var new_msg = [];
 
 		if (select != "post") {
-			var new_msg = document.createElement("li");
-			new_msg.onclick = function() {
+			new_msg.push(document.createElement("li"));
+		}
+		new_msg.push(document.createElement("li"));
+
+		for (var i = 0; i < new_msg.length; i++) {
+			new_msg[i].addEventListener('click', function(e) {
 				document.getElementById("msg_info_title").innerHTML = title;
 				document.getElementById("msg_info_text").innerHTML = text;
 				document.getElementById("msg_info_return").style.display = "block";
 				document.getElementById("add").style.display = "none";
 				document.getElementById("outer_wrap").style.right = "100%";
 				document.getElementById("msg_info").style.left = "0";
-			}
-			new_msg.innerHTML = "<h1>" + title + "</h1>\n<h3>" + text + "</h3><button value='" + snap.key + "' class='remove_post'>Remove</button>";
-			document.getElementById("post").insertBefore(new_msg,
-			 document.getElementById("post").firstChild);
+			}, false);
+			new_msg[i].innerHTML = "<h1>" + title + "</h1>\n<h3>" + text + "</h3><button onclick='remove_post(\"" + snap.key + "\")' value='" + snap.key + "' class='remove_post'>Remove</button>";
 		}
-		var new_msg = document.createElement("li");
-		new_msg.onclick = function() {
-			document.getElementById("msg_info_title").innerHTML = title;
-			document.getElementById("msg_info_text").innerHTML = text;
-			document.getElementById("msg_info_return").style.display = "block";
-			document.getElementById("add").style.display = "none";
-			document.getElementById("outer_wrap").style.right = "100%";
-			document.getElementById("msg_info").style.left = "0";
+		if (select != "post") {
+			document.getElementById(select).insertBefore(new_msg[0], document.getElementById(select).firstChild);
 		}
-		new_msg.innerHTML = "<h1>" + title + "</h1>\n<h3>" + text + "</h3><button value='" + snap.key + "' class='remove_post'>Remove</button>";
-		document.getElementById(select).insertBefore(new_msg,
-			 document.getElementById(select).firstChild);
+		document.getElementById("post").insertBefore(new_msg[1], document.getElementById("post").firstChild);
 	});
 }
