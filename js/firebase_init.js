5f466384e459563e8548c1e2946e583dbe1874e8
theunderpaidone <antonya34@gmail.com>
2017-02-24 23:18:24 -0600

    some firebase stuff starter pack

---
+++ js/firebase_init.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDFoH-LzsCuQzND-_74g0EA4BpYZN10tKM",
+    authDomain: "hackhub-255d7.firebaseapp.com",
+    databaseURL: "https://hackhub-255d7.firebaseio.com",
+    storageBucket: "hackhub-255d7.appspot.com",
+    messagingSenderId: "157801572889"
+};
+firebase.initializeApp(config);
